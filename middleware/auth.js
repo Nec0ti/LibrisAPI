9bd127685610e1a359c5025231e51caaa19605a5
Nec0ti <140799408+Nec0ti@users.noreply.github.com>
2025-03-03 19:48:31 +0300

    asdasd

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = function(req, res, next) {
   const token = req.header('x-auth-token');
 
   if (!token) {
-    return res.status(401).json({ message: 'Yetkilendirme başarısız, token yok' });
+    return res.status(401).json({ message: 'Authorization failed, no token' });
   }
 
   try {
@@ -12,6 +12,6 @@ module.exports = function(req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Token geçersiz' });
+    res.status(401).json({ message: 'Invalid Token' });
   }
 };
--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,7 +1,7 @@
 module.exports = function(role) {
   return (req, res, next) => {
     if (req.user.role !== role) {
-      return res.status(403).json({ message: 'Yetkisiz erişim' });
+      return res.status(403).json({ message: 'Unuthorized Access' });
     }
     next();
   };

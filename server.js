6f43ee1e55863a752f335fa738aaadb3f65ac4b7
Nec0ti <140799408+Nec0ti@users.noreply.github.com>
2025-03-03 20:57:06 +0300

    limit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const booksRoute = require('./routes/books');
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
@@ -9,8 +10,17 @@ const usersRoute = require('./routes/users');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'You requested too much! PLEASE STOP!',
+  standardHeaders: true,
+	legacyHeaders: false,
+});
 
+app.use(limiter);
+
+app.use(cors());
 app.use(express.json());
 
 const mongoDBUri = process.env.MONGODB_URI;
